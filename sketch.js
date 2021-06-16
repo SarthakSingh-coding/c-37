40014935cf87760628035f85cce51eccdfe448dd
SarthakSingh-coding <77876549+SarthakSingh-coding@users.noreply.github.com>
2021-06-16 20:00:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var database;
+var position;
+var gameState = 0;
+var playerCount;
+var form, game, player;
+var allPlayers;
+var car1,car2,car3,car4,cars;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(displayWidth-20, displayHeight-30);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+
+  if(playerCount === 4){
+    game.update(1);
+  }
+
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+
+
+    
+}
+
+
+
